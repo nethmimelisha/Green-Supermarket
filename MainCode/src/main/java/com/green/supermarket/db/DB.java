7f8f4351dcd7ee372f6264dcce4040a2ff01a5ef
nethmimelisha <121291124+nethmimelisha@users.noreply.github.com>
2023-12-19 16:46:56 +0530

    User Database

--- MainCode/src/main/java/com/green/supermarket/db/DB.java
+++ MainCode/src/main/java/com/green/supermarket/db/DB.java
@@ -5,11 +5,11 @@ import java.sql.*;
 public class DB {
     static public Connection con = null;
 
-    public_static Connection getCon() throws Exception
+    public static Connection getCon() throws Exception
     {
         if (con == null)
         {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/green_supermarket";
             con = DriverManager.getConnection(url, "root","Nethmi@123");
         }
